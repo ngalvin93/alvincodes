562e6140352a4ffe9ae8579e7350b9f63c9d995a
Alvin Ng <32407008+ngalvin93@users.noreply.github.com>
2023-01-16 21:45:35 -0600

    start project thumbs

--- src/components/PortfolioThumb.js
+++ src/components/PortfolioThumb.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+import styles from '../styles/PortfolioThumb.module.css';
+
 export default function PortfolioThumb(props) {
     return (
-        <div>{props.name}</div>
+        <div className={styles.container}>
+            <Image
+                className={styles.thumbnail}
+                src='/images/react-battleship/bs-progress.jpg'
+                width={900}
+                height={600}
+                alt='React Battleship'
+            />
+            <div className={styles.title}>{props.name}</div>
+        </div>
     )
 }
